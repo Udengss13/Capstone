0d956d9c50e5a99692bc512abee73a1b82a2dbc9
Melody <melody.santiago.b@bulsu.edu.ph>
2022-05-25 17:49:36 +0800

    update

--- CAPSTONE/admin/index.php
+++ CAPSTONE/admin/index.php
@@ -50,5 +50,48 @@
 		</header>
 	</div>
 
+	<?php
+    session_start();
+?>
+   <header class="bg">
+    <nav class="navbar navbar-style">
+      <div class="container" >
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+
+          </button>
+          <a href=""><img class="logo" src="logo.png"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="micon" >
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="">home</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="#imagesec">Gallery</a></li>
+          <li><a href="#about">Contact Us</a></li>
+          <div class="dropdown">
+
+
+<div class="dropdown">
+  <button class="dropbtn">Login</button>
+  <div class="dropdown-content">
+  <a href="login.php">User</a>
+  <a href="admin\admin_login.php">Administrator</a>
+  
+  </div>
+</div>
+
+        </ul>
+        </div>
+      </div>
+    </nav>
+>
+
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 </html>
--- CAPSTONE/admin/login_admin_process.php
+++ CAPSTONE/admin/login_admin_process.php
@@ -7,23 +7,23 @@
         $user = mysqli_real_escape_string($conn, $_POST['username']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
     
-        $sql = "SELECT * FROM admin_db WHERE BINARY username = '$user'  and BINARY password='$password'";
+        $sql = "SELECT * FROM `admin_db` WHERE BINARY username = '$user'  and BINARY password='$password'";
         $result = mysqli_query($conn, $sql);
         $count = mysqli_num_rows($result);
 
         if($count == 1){
                 
-                   $_SESSION['user'] = $user;
-                   $_SESSION['pass'] = $password;
+                   $_SESSION['username'] = $user;
+                   $_SESSION['password'] = $password;
                   
-                   header("location: account.php");  
+                   header("location: index.php");  
              } 
              else{
-                    header("location: login.php?loginerror=".$user);
+                    header("location: login_admin.php?loginerror=".$user);
             }
     }
     else{
-        header("location: login.php?loginerror=".$user);
+        header("location: login_admin.php?loginerror=".$user);
 }
             
    
